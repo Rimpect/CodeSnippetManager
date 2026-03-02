ebce8c8b67f17dcb92ce2f59ead5783af9ae95ba
Maks <gitlaborhub@gmail.com>
2026-03-02 10:49:55 +0300

    сверстан dashboard

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       {/* <DashboardPage /> */}
-      <CodeEditor />
+      <DashboardPage />
     </>
   );
 }
--- client/src/widgetes/SnippetItem/SnippetItem.jsx
+++ client/src/widgetes/SnippetItem/SnippetItem.jsx
@@ -4,9 +4,26 @@ export default function SnippetItem(props) {
   const { id, title, description, tags } = props;
   return (
     <li className="snippet__item">
-      <div className="snippet__icon">📄</div>
+      <div className="snippet__icon">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-code-xml size-5 text-blue-600"
+        >
+          <path d="m18 16 4-4-4-4"></path>
+          <path d="m6 8-4 4 4 4"></path>
+          <path d="m14.5 4-5 16"></path>
+        </svg>
+      </div>
 
-      <div className="snippet__content"> 
+      <div className="snippet__content">
         <div className="snippet__header">
           <div className="snippet__title">{title}</div>
           <div className="snippet__id">{id}</div>

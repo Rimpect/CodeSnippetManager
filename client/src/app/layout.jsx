19f7e7d163a4e5c9dc2e91717c0e268757bcf9fc
Rimpect <gitlaborhub@gmail.com>
2026-03-23 22:59:47 +0300

    исправлено добавление сниппета

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,10 +1,14 @@
 import Header from "../widgetes/Header/Header";
 import { Outlet } from "react-router-dom";
 
-function Layout({ searchQuery, setSearchQuery }) {
+function Layout({ searchQuery, setSearchQuery, onCreateClick }) {
   return (
     <div>
-      <Header searchQuery={searchQuery} onSearchChange={setSearchQuery} />
+      <Header
+        searchQuery={searchQuery}
+        onSearchChange={setSearchQuery}
+        onCreateClick={onCreateClick}
+      />
       <main>
         <Outlet context={{ searchQuery }} />
       </main>
